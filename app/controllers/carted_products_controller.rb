602aa74ab643befa2c41678369d8330c3577d4f9
amberdemirlika <amberdemir12@gmail.com>
2024-10-09 20:40:43 -0500

    add carted_orders

---
+++ app/controllers/carted_products_controller.rb
@@ -0,0 +1,25 @@
+class CartedProductsController < ApplicationController
+  def index
+    @carted_products = CartedProduct.where(status: "carted", user_id: current_user.id)
+    render :index
+  end
+
+  def create
+    @carted_products = CartedProduct.create(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: "carted",
+      order_id: nil,
+    )
+    @carted_products.save
+    render :show
+  end
+
+  def destroy
+    @carted_product = CartedProduct.find_by(id: params[:id])
+    @carted_product.status = "removed"
+    @carted_product.save
+    render json: { mesage: "item removed" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
 
   post "/orders" => "orders#create"
 
+  get "/carted_products" => "carted_products#index"
+  post "/carted_products" => "carted_products#create"
+  delete "/carted_products/:id" => "carted_products#destroy"
+
   # get "/users" => "user#index"
   # post "/users" => "users#create"
 
